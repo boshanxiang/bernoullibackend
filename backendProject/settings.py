ea619340b08d930fcf58afa89d193ed33970a1ba
Bo Xiang <boshan.xiang@gmail.com>
2021-07-13 10:22:26 -0500

    update env

--- backendProject/settings.py
+++ backendProject/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
+from .sec import *
 
 from pathlib import Path
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l5chf)^8)8w@9!lhwg3bw@*i*3%0l1^&&3+ww$kmy@yzy0=czo'
+SECRET_KEY = SecretKey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
